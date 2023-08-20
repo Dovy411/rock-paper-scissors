e21bfc3af63a075af7bc0925d2260542ac77d56a
Dovy411 <dovydas4111@gmail.com>
2023-08-20 16:34:38 +0100

    Added getPlayerChoice()

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
 function getComputerChoice(){
     const items = ["rock","paper","scissors"]
     var computerChoice = items[Math.floor(Math.random()*items.length)];
-    return computerChoice
+    return computerChoice;
 }
 
 console.log("Computer chooses: ",getComputerChoice());
+
+function getPlayerChoice(){
+    var playerChoice = "Rock";
+    return playerChoice.toLowerCase();
+}
+
+console.log("Player chooses: ",getPlayerChoice());
