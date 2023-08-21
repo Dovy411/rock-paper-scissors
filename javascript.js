076cc53b1eb307e2e46593a8ba58be93eabc2c70
Dovy411 <dovydas4111@gmail.com>
2023-08-21 19:52:19 +0100

    Added playRound() and edited previous functions

--- javascript.js
+++ javascript.js
@@ -1,14 +1,33 @@
 function getComputerChoice(){
     const items = ["rock","paper","scissors"]
     var computerChoice = items[Math.floor(Math.random()*items.length)];
+    console.log("Computer chooses: ",computerChoice);
     return computerChoice;
 }
 
-console.log("Computer chooses: ",getComputerChoice());
+
 
 function getPlayerChoice(){
     var playerChoice = "Rock";
+    console.log("Player chooses: ",playerChoice.toLowerCase());
     return playerChoice.toLowerCase();
 }
 
-console.log("Player chooses: ",getPlayerChoice());
+
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "rock") {
+        console.log("1")
+        return "DRAW.";
+    }   else if (playerChoice === "rock" && computerChoice === "paper") {
+            console.log("2")
+            return "You lost. Paper beats rock!";
+        }
+        else if (playerChoice === "rock" && computerChoice === "scissors"){
+            console.log("3")
+            return "You won. Rock beats scissors.";
+        }
+}
+
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
