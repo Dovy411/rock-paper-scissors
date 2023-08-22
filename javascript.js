d74f92c94326dcadf73ec684379ba136bc1b9e08
Dovy411 <dovydas4111@gmail.com>
2023-08-22 22:29:46 +0100

    Added the other player choices and score keeping

--- javascript.js
+++ javascript.js
@@ -8,26 +8,67 @@ function getComputerChoice(){
 
 
 function getPlayerChoice(){
-    var playerChoice = "Rock";
+    var playerChoice = "rock";
     console.log("Player chooses: ",playerChoice.toLowerCase());
     return playerChoice.toLowerCase();
 }
 
+let draws = 0;
+let playerWins = 0;
+let computerWins = 0;
 
 
 function playRound(playerChoice, computerChoice) {
     if (playerChoice === "rock" && computerChoice === "rock") {
-        console.log("1")
+        console.log("1,1")
+        draws ++;
         return "DRAW.";
     }   else if (playerChoice === "rock" && computerChoice === "paper") {
-            console.log("2")
+            console.log("1,2")
+            computerWins ++;
             return "You lost. Paper beats rock!";
         }
         else if (playerChoice === "rock" && computerChoice === "scissors"){
-            console.log("3")
+            console.log("1,3")
+            playerWins ++;
             return "You won. Rock beats scissors.";
+        }
+        else if (playerChoice === "paper" && computerChoice === "rock") {
+            console.log("2,1")
+            playerWins ++;
+            return "You won. Paper beats rock!";
+        }
+        else if (playerChoice === "paper" && computerChoice === "paper") {
+            console.log("2,2")
+            draws ++;
+            return "DRAW.";
+        }
+        else if (playerChoice === "paper" && computerChoice === "scissors") {
+            console.log("2,3")
+            computerWins ++;
+            return "You lost. Scissors beat paper.";
+        }
+        else if (playerChoice === "scissors" && computerChoice === "rock") {
+            console.log("3,1")
+            computerWins ++;
+            return "You lost. Rock beats scissors.";
+        }
+        else if (playerChoice === "scissors" && computerChoice === "paper") {
+            console.log("3,2")
+            playerWins ++;
+            return "You won. Scissors beat paper.";
+        }
+        else if (playerChoice === "scissors" && computerChoice === "scissors") {
+            console.log("3,3")
+            draws ++;
+            return "DRAW.";
+        }
+        else {
+            return "invalid input from player"
         }
 }
 
 
+
 console.log(playRound(getPlayerChoice(), getComputerChoice()));
+
